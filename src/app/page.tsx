56a79ab222bab57e4dac2c809321a5de4dd5645b
Mohit Thapa <thapamohit2058@gmail.com>
2025-04-27 10:01:37 +0545

    add schema for upload and fetch data from convex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,14 @@
 
 import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, SignOutButton} from "@clerk/nextjs";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 
 export default function Home() {
 
-
+const createFile = useMutation(api.files.createFile)
+const files = useQuery(api.files.getFile)
   return (
    <main className="flex min-h-screen flex-col items-center justify-center">
     <SignedIn>
@@ -16,6 +19,15 @@ export default function Home() {
       <SignInButton mode="modal">
       <Button>Sign In</Button></SignInButton> 
     </SignedOut>
+
+    {files?.map(file => {
+      return <div key={file._id}>{file.name}</div>
+    })}
+    <Button onClick={() => {
+      createFile({
+        name:"Hello World"
+      })
+    }}>CLick Me</Button>
    
   
    </main>
