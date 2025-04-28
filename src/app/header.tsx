49f62f4ba995c2284430d1f53356b5e8537904b2
Mohit Thapa <thapamohit2058@gmail.com>
2025-04-28 23:20:28 +0545

    add clerk webhook with convex

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,4 +1,5 @@
-import { OrganizationSwitcher, UserButton } from '@clerk/nextjs'
+import { Button } from '@/components/ui/button'
+import { OrganizationSwitcher, SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 export function Header() {
@@ -6,10 +7,17 @@ export function Header() {
     <div className=' border-b py-4 bg-gray-50'>
         <div className=' items-center container mx-auto justify-between flex'>
             SkyKeep
+            <SignedIn>
             <div className='flex gap-2'>
                 <OrganizationSwitcher />
             <UserButton />
             </div>
+            </SignedIn>
+            <SignedOut>
+              <SignInButton>
+                <Button>Sign In</Button>
+              </SignInButton>
+            </SignedOut>
             
 
         </div>
